4daf0ae190fa9424726b70881732934597c35ec2
ustyushkin <ustyushkin.vyacheslav@gmail.com>
2018-05-21 22:52:55 +0300

    next stage

---
+++ app/Http/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: yustas
+ * Date: 20.05.18
+ * Time: 22:01
+ */
+
+namespace App\Http;
+
+
+class User
+{
+
+}
---
+++ app/UserModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: yustas
+ * Date: 21.05.18
+ * Time: 22:05
+ */
+
+namespace App;
+
+
+class UserModel
+{
+
+}
